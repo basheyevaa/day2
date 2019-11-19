6703d6da6d42d5f2f959c103bcdb98112040cb2a
Alima Basheyeva <57907623+basheyevaa@users.noreply.github.com>
2019-11-19 19:22:39 +0600

    9.cpp

---
+++ 9.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,a1,a2,a3,a4;
+        cin>>a;
+        if(a<9999 && a>999){
+            a4=a%10;
+            a3=(a%100-a4)/10;
+            a2=(a%1000-a%100)/100;
+            a1=(a-a%1000)/1000;
+            if(a1>a2 && a2>a3 && a3>a4){
+                cout<<"yes";
+            }
+            else {
+                cout<<"no";
+        }
+        }
+        else {
+            cout<<"Not 4 digit number";
+        }
+    return 0;
+}
