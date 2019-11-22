99cda9a78265e9ca82561a7d375ecbce8c4d3d53
Alima Basheyeva <57907623+basheyevaa@users.noreply.github.com>
2019-11-23 02:23:00 +0600

    127.cpp

---
+++ 127.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	int end = sqrt(n);
+	for (int i = 2; i <= end; ++i) {
+		if (n % i == 0) {
+			cout << "no" << endl;
+			return 0;
+		}
+	}
+	cout << "yes" << endl;
+}
